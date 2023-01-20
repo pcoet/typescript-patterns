641d2837f85f3669f6b641ca7fb3d489da08fd08
David Huntsperger <dhuntsperger@google.com>
2023-01-20 06:13:54 -0800

    added minimal express server

---
+++ src/web/app.ts
@@ -0,0 +1,31 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import express, { Express } from 'express';
+
+/*
+ * To run the app: `npm run build && node dist/web/app.js`
+ */
+const app: Express = express();
+const port = 3000;
+
+app.use(express.static('static'))
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
